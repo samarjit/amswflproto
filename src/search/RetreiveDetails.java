1676b98f93029b926efc453939fc7956458ce010
justinjose12002 <justinjose12002@20d71b38-bf2e-11de-a8ef-132414f8baa6>
2009-11-03 18:14:39 +0000

    added retrivedetails

---
+++ src/search/RetreiveDetails.java
@@ -0,0 +1,80 @@
+package search;
+
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.sql.rowset.CachedRowSet;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.interceptor.RequestAware;
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import dbconn.DBConnector;
+
+public class RetreiveDetails extends ActionSupport implements ServletRequestAware{
+
+	private InputStream inputStream;
+	private String panelFields1WhereClause;
+	private HttpServletRequest servletRequest;
+	
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+    
+    public HttpServletRequest getServletRequest() {
+		return servletRequest;
+	}
+
+	 
+
+	public String execute() throws Exception {
+    	
+    	HashMap metadata = new HashMap();
+    	RetreiveData retrive = new RetreiveData();
+    	
+    	if(servletRequest!=null)
+    	System.out.println("query string RD : " + servletRequest.getQueryString());
+    	
+    	HttpServletRequest request1 =  ServletActionContext.getRequest();
+    	System.out.println(request1.getQueryString());
+    	
+    	
+    	//panelFields1WhereClause = request1.getParameter("panelFields1WhereClause");
+    	panelFields1WhereClause = request1.getParameter("amp;panelFields1WhereClause");
+    	
+    	System.out.println(panelFields1WhereClause);	
+    	
+    	String resultHtml  = retrive.doRetrieveData(panelFields1WhereClause);
+    	
+    	
+        System.out.println(panelFields1WhereClause);
+       // String resXML  = getResultXML (qry,metadata); 
+        inputStream = new StringBufferInputStream(resultHtml);
+    	//inputStream = new StringBufferInputStream("in view details");
+        System.out.println("in view details");
+        return SUCCESS;
+    }
+	
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		servletRequest = request;
+		
+	}
+
+
+	 
+
+}
