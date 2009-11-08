27e15c54041506efc9a14d2575804d451c8db2ae
samarjit.samanta <samarjit.samanta@20d71b38-bf2e-11de-a8ef-132414f8baa6>
2009-11-08 14:57:54 +0000

    ScreenFlow

--- src/workflow/ScreenFlow.java
+++ src/workflow/ScreenFlow.java
@@ -1,13 +1,268 @@
 package workflow;
 
-public class ScreenFlow {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class ScreenFlow {
+	boolean initialized = false;
+	public ScreenFlow() {
+		super();
+		if(!initialized)init();
+	}
+	private static HashMap<String,String> workflowlocationcache = null; 
+	
+	public Document parserXML(String file) throws SAXException, IOException, ParserConfigurationException
+	{
+		return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
+	}
+ 
+private void init(){
+	if(initialized)return;
+	initialized = true;
+	URL  st =  ClassLoader.getSystemResource("scrflowxml/screenflowfactory.xml");
+	Document doc = null;
+	workflowlocationcache = new HashMap<String, String>();
+	try {
+		doc = parserXML(st.getFile());
+		
+	 	NodeList nl = doc.getElementsByTagName("screenflow");
+          for(int i = 0 ;i<nl.getLength();i++){
+        	  Node node = nl.item(i);
+        	  if(node.getNodeType() == Document.ELEMENT_NODE){
+        		   Element elm = (Element) node;
+        		   String key = elm.getAttribute("key");
+        		   String val = elm.getAttribute("value");
+        		   workflowlocationcache.put(key	, val);
+        	  }
+          }
+  
+	} catch (SAXException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (ParserConfigurationException e) {
+		e.printStackTrace();
+	}
+}
+
+public ArrayList<String> getNextActions(String scrFlowName, String currentAction){
+	ArrayList<String> retar = new ArrayList<String>();
+	String workflowFile = 	ScreenFlow.workflowlocationcache.get(scrFlowName);
+	String transitTo ="";
+	try {
+		URL url = ScreenFlow.class.getResource(workflowFile);
+		if(url == null) throw new IOException("File not found");
+		Document doc = parserXML(url.getFile());
+		NodeList nl = doc.getElementsByTagName("state");
+			if(currentAction==null || "".equals(currentAction)){
+				NodeList nlstart = doc.getElementsByTagName("start-state");
+				Element firstNode = (Element)nlstart.item(0);
+				retar.add(firstNode.getAttribute("name"));
+				/* NodeList descNodes = firstNode.getChildNodes();
+				  for(int i=0;i<descNodes.getLength();i++){
+					  if(descNodes.item(i).getNodeType() == Document.ELEMENT_NODE){
+						  Element desElm = (Element) descNodes.item(i);
+						  if(desElm.getNodeName().equals("description")){
+							  if(desElm.getTextContent() != null  &&   desElm.getTextContent().length() > 1){
+								  System.out.println(desElm.getTextContent().trim());
+							  retar.add (desElm.getTextContent().trim());
+							  }
+							  
+						  }
+					  }
+				  }*/
+			}else{ //if already start state
+				NodeList nlstart = doc.getElementsByTagName("start-state");
+				Element firstNode = (Element)nlstart.item(0);
+				Element currentElement = null;
+				boolean found = false; 
+				 if(currentAction.equals(firstNode.getAttribute("name"))){
+					 currentElement = firstNode;
+					 found=true;
+				 }
+				 
+				 
+				
+				XPath xpath = XPathFactory.newInstance().newXPath();
+				if( !found ){ 
+					NodeList nlstate = (NodeList) xpath.evaluate("process-definition/state", doc, XPathConstants.NODESET);
+					//parsing all the states amd match names with  currentAction
+					for(int i =0 ;i<nlstate.getLength();i++){
+						Node node =  nlstate.item(i);
+						if(node.getNodeType() == Document.ELEMENT_NODE){
+							Element elm = (Element) node;
+							if(currentAction.equals(elm.getAttribute("name"))){
+								currentElement = elm;
+								 found=true; break;
+							}
+						}
+					}
+				}
+				
+				if( !found ){ 
+					NodeList nlstate = (NodeList) xpath.evaluate("process-definition/task-node", doc, XPathConstants.NODESET);
+					for(int i =0 ;i<nlstate.getLength();i++){
+						Node node =  nlstate.item(i);
+						if(node.getNodeType() == Document.ELEMENT_NODE){
+							Element elm = (Element) node;
+							if(currentAction.equals(elm.getAttribute("name"))){
+								currentElement = elm;
+								 found=true; break;
+							}
+						}
+					}
+				}
+				
+				if( !found ){ 
+					NodeList nlstate = (NodeList) xpath.evaluate("process-definition/fork", doc, XPathConstants.NODESET);
+					for(int i =0 ;i<nlstate.getLength();i++){
+						Node node =  nlstate.item(i);
+						if(node.getNodeType() == Document.ELEMENT_NODE){
+							Element elm = (Element) node;
+							if(currentAction.equals(elm.getAttribute("name"))){
+								currentElement = elm;
+								 found=true; break;
+							}
+						}
+					}
+				}
+				
+				//decision is very important will work on it later to implement handlers
+				if( !found ){ 
+					NodeList nlstate = (NodeList) xpath.evaluate("process-definition/decision", doc, XPathConstants.NODESET);
+					for(int i =0 ;i<nlstate.getLength();i++){
+						Node node =  nlstate.item(i);
+						if(node.getNodeType() == Document.ELEMENT_NODE){
+							Element elm = (Element) node;
+							if(currentAction.equals(elm.getAttribute("name"))){
+								currentElement = elm;
+								 found=true; break;
+							}
+						}
+					}
+				}
+				
+				 if( !found ){
+					 NodeList nlstate = doc.getElementsByTagName("node");
+					 for(int i =0 ;i<nlstate.getLength();i++){
+							Node node =  nlstate.item(i);
+							if(node.getNodeType() == Document.ELEMENT_NODE){
+								Element elm = (Element) node;
+								if(currentAction.equals(elm.getAttribute("name"))){
+									currentElement = elm;
+									 found=true; break;
+								}
+							}
+						}
+					 
+				 }
+				 
+				 if( !found ){ 
+						NodeList nlstate = (NodeList) xpath.evaluate("process-definition/join", doc, XPathConstants.NODESET);
+						for(int i =0 ;i<nlstate.getLength();i++){
+							Node node =  nlstate.item(i);
+							if(node.getNodeType() == Document.ELEMENT_NODE){
+								Element elm = (Element) node;
+								if(currentAction.equals(elm.getAttribute("name"))){
+									currentElement = elm;
+									 found=true; break;
+								}
+							}
+						}
+					}
+				 
+				 if( !found ){
+					 retar.add("Element Not Found");
+					 throw new Exception("element not found");
+				 }
+				  NodeList descNodes = currentElement.getChildNodes();
+				  for(int i=0;i<descNodes.getLength();i++){
+					  if(descNodes.item(i).getNodeType() == Document.ELEMENT_NODE){
+						  Element desElm = (Element) descNodes.item(i);
+						  if(desElm.getNodeName().equals("transition")){
+							  if(desElm.getAttribute("to") != null  &&   desElm.getAttribute("to").length() > 1){
+								  transitTo = desElm.getAttribute("to");
+								  retar.add (transitTo.trim());
+								  
+							  }
+							  
+						  }
+					  }
+				  }
+				      
+			}
+	} catch (SAXException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (ParserConfigurationException e) {
+		e.printStackTrace();
+	} catch (XPathExpressionException e) {
+		e.printStackTrace();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+	return retar;
+}
+
+
+public String getActionScreenName(String scrFlowName,String currentAction){
+	String workflowFile = 	ScreenFlow.workflowlocationcache.get(scrFlowName);
+	String transitTo ="";
+	String screenName = "";
+	boolean  found = false;
+	try {
+		URL url = ScreenFlow.class.getResource(workflowFile);
+		if(url == null) throw new IOException("File not found");
+		Document doc = parserXML(url.getFile());
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		NodeList nlstate = (NodeList) xpath.evaluate("/process-definition/state[@name=\""+currentAction+"\"]/description", doc, XPathConstants.NODESET);
+		if(nlstate != null ){
+			found = true;
+			Node descnode = nlstate.item(0);
+			if(descnode != null){
+				screenName = descnode.getTextContent();
+				screenName = screenName.trim();
+			}else{
+				screenName = "no screen mapped";	
+			}
+		}
+		
+	}catch(IOException e){
+		e.printStackTrace();
+	} catch (SAXException e) {
+		e.printStackTrace();
+	} catch (ParserConfigurationException e) {
+		e.printStackTrace();
+	} catch (XPathExpressionException e) {
+		e.printStackTrace();
+	}
+	
+	return screenName;
+}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ScreenFlow scf = new ScreenFlow();
+		scf.init();
+		System.out.println(scf.getNextActions("loginflow", "start"));
 	}
 
 }
