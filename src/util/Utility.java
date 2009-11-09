b91b7f0b879360b69c9e6dda0aa183db53197401
justinjose12002 <justinjose12002@20d71b38-bf2e-11de-a8ef-132414f8baa6>
2009-11-09 12:35:06 +0000

    added json support

--- src/util/Utility.java
+++ src/util/Utility.java
@@ -1,6 +1,11 @@
 package util;
 
 import java.util.HashMap;
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Utility {
 	/**
@@ -20,18 +25,60 @@ public class Utility {
 		return whereClausePart;
 	}
 	
-	public static HashMap<String, String> extractKeyValPair(String str/*String whereStringOfPanel String panelName */){
+	public static HashMap<String, HashMap<String, String>> extractKeyValPair(String str/*String whereStringOfPanel String panelName */) throws JSONException{
 		//"whereClausefrmRequest"
 		
-		String strWhereClause = str;
+		String panleName = null;
+		JSONObject injsonobj;
+		JSONObject innerjsonobj;
+
+		HashMap<String, HashMap<String, String>> whereClause = new HashMap<String, HashMap<String,String>>();
+
+		//Sample string
+		//String jsonStr = "{\"json\":[{\"name\":\"panelFields\",\"valuesar\":[{\"key\":\"bdate\",\"value\":\"sd\"},{\"key\":\"empid\",\"value\":\"sd\"},{\"key\":\"empname\",\"value\":\"\"},{\"key\":\"empemail\",\"value\":\"\"}]},{\"name\":\"panelFields1\",\"valuesar\":[{\"key\":\"bdate\",\"value\":\"\"},{\"key\":\"empid\",\"value\":\"\"},{\"key\":\"empname\",\"value\":\"\"},{\"key\":\"empemail\",\"value\":\"\"}]},{\"name\":\"panelFields2\",\"valuesar\":[{\"key\":\"bdate\",\"value\":\"\"},{\"key\":\"empid\",\"value\":\"\"},{\"key\":\"empname\",\"value\":\"\"},{\"key\":\"empemail\",\"value\":\"\"}]}]}";
+
+		JSONObject json = new JSONObject(str);
+		JSONArray injson = (JSONArray) json.get("json") ;
+
+		for(int i = 0; i< injson.length(); i++ ){
+			injsonobj = (JSONObject) injson.get(i);
+			Iterator itjobj = injsonobj.keys();
+			while(itjobj.hasNext()){
+				//System.out.println(injsonobj.get(itjobj.next().toString()) instanceof JSONObject);
+				HashMap<String, String> valueKey = new HashMap<String, String>();
+				panleName = injsonobj.get(itjobj.next().toString()).toString();
+				//System.out.println(panleName);
+				JSONArray innerjson = (JSONArray) injsonobj.get(itjobj.next().toString());
+				for(int j = 0; j< innerjson.length(); j++ ){
+					innerjsonobj = (JSONObject) innerjson.get(j);
+					Iterator itinjobj = innerjsonobj.keys();
+					while(itinjobj.hasNext()){    
+						//System.out.println(ii);						
+						//System.out.println("+++++++++++==  " + itinjobj.next().toString());
+						//System.out.println(itinjobj.next().toString());
+						//System.out.println(itinjobj.next().toString());
+						String value = innerjsonobj.get(itinjobj.next().toString()).toString();
+						String key = innerjsonobj.get(itinjobj.next().toString()).toString();						
+						valueKey.put(key, value);
+					}
+				}
+				//System.out.println(injsonobj.get(itjobj.next().toString() instanceof JSONArray);
+				whereClause.put(panleName, valueKey);				
+			}
+			
+		}				
+		
+	/*	String strWhereClause = str;
 		String[] arWhere = strWhereClause.split("~#");
 		HashMap<String,String> whereClausePart = new HashMap<String, String>();
 		for(String strWhrKVpair:arWhere){
 			String[] kvpair = strWhrKVpair.split("!");
 			if(kvpair[0] !=null )
 			whereClausePart.put(kvpair[0].toLowerCase(), kvpair[1]);
-		}
-		return whereClausePart;
+		}*/
+		
+		
+		return whereClause;
 	}
 	
 }
